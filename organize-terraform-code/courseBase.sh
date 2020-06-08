27b64ca1954e6a077853568dfb1ec240c7ec997b
Robin Norwood <rnorwood@hashicorp.com>
2020-06-08 09:58:55 -0500

    Set up first step

--- organize-terraform-code/courseBase.sh
+++ organize-terraform-code/courseBase.sh
@@ -1,24 +1,20 @@
+## Remove when using hashicorp image:
+
 TF_VER=0.12.26
 
-#mkdir -p ~/src
+apt-get --assume-yes install unzip
+apt-get --assume-yes install awscli
 
-apt-get install unzip
-#cd ..
 curl -O "https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_linux_amd64.zip"
 unzip "terraform_${TF_VER}_linux_amd64.zip" -d /usr/local/bin/
 
+git clone https://github.com/hashicorp/learn-terraform-modules.git
+cd learn-terraform-modules
+git checkout tags/ec2-instances -b ec2-instances
 
-# cd ~/src && curl -L https://github.com/hashicorp/katakoda/raw/master/consul-connect/assets/bin/counting-service -O
-# mv ~/src/counting-service /usr/local/bin
-# cd ~/src && curl -L https://github.com/hashicorp/katakoda/raw/master/consul-connect/assets/bin/dashboard-service -O
-# mv ~/src/dashboard-service /usr/local/bin
-# chmod +x /usr/local/bin/*-service
+## End
 
-# useradd consul --create-home
-# mkdir -p /etc/consul.d
-# cd /etc/consul.d && curl -L https://github.com/hashicorp/katakoda/raw/master/consul-connect/assets/config/counting.json -O
-# cd /etc/consul.d && curl -L https://github.com/hashicorp/katakoda/raw/master/consul-connect/assets/config/dashboard.json -O
-# mkdir -p /home/consul/log
-# chown -R consul /home/consul
+# useradd terraform --create-home
+# chown -R terraform /home/terraform
+
 # echo '127.0.0.1 localhost' >> /etc/hosts
-# runuser -l consul -c "consul agent -dev -client 0.0.0.0 -config-dir=/etc/consul.d >/home/consul/log/consul.log 2>&1 &"
--- organize-terraform-code/step1.md
+++ organize-terraform-code/step1.md
@@ -1,2 +1,19 @@
-# The first step
+# Using Terraform modules
+
+First, configure AWS for this scenario:
+
+```
+aws configure
+```{{execute}}
+
+You will be prompted to enter the AWS Key ID and Secret Key provided for this
+lab. Use `us-east-1`, or your preferred AWS region, as the region.
+
+```
+$ aws configure
+AWS Access Key ID [None]:  AKIA#########OIEU
+AWS Secret Access Key [None]: Kg7W#################RMIyGv
+Default region name [None]: us-east-1
+Default output format [None]:
+```
 
