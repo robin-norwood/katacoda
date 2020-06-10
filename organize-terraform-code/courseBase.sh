bbda99559acbbf6f0eda01eccca3e88e2d552aab
Robin Norwood <rnorwood@hashicorp.com>
2020-06-10 14:24:29 -0500

    Refactor courseBase.sh, change prompt to show current directory

--- organize-terraform-code/courseBase.sh
+++ organize-terraform-code/courseBase.sh
@@ -2,19 +2,15 @@
 
 TF_VER=0.12.26
 
-# apt-get --assume-yes install unzip
-
 curl -O "https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_linux_amd64.zip"
 unzip "terraform_${TF_VER}_linux_amd64.zip" -d /usr/local/bin/
 
+## End remove
+
 mkdir -p learn-terraform/assets
-#cp /root/index.html /root/learn-terraform/assets/
 cd learn-terraform
 touch {main.tf,variables.tf,outputs.tf}
 
-## End
+PS1='\W$ '
 
-# useradd terraform --create-home
-# chown -R terraform /home/terraform
-
-# echo '127.0.0.1 localhost' >> /etc/hosts
+clear
