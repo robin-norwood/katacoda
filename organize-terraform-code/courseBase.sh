2521f79048a842a55a79d32ea889f0c8bfe79ae3
Robin Norwood <rnorwood@hashicorp.com>
2020-06-10 16:58:31 -0500

    Alias for yes command

--- organize-terraform-code/courseBase.sh
+++ organize-terraform-code/courseBase.sh
@@ -11,6 +11,10 @@ mkdir -p learn-terraform/assets
 cd learn-terraform
 touch {main.tf,variables.tf,outputs.tf}
 
+# Include current dir in prompt
 PS1='\W$ '
+
+# Prevent `yes` command from accidentally being run
+alias yes=""
 
 clear
